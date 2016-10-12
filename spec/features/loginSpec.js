c0e3069d4850a95a4a65b10fb046195d859c50da
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2016-10-12 15:40:05 -0600

    Add tests for logout

--- spec/features/loginSpec.js
+++ spec/features/loginSpec.js
@@ -6,12 +6,19 @@ Browser.localhost('example.com', 3000);
 
 describe('login', function() {
 
-  const browser = new Browser();
+  var browser, agent;
 
   beforeEach(function(done) {
+    browser = new Browser();
     models.sequelize.sync({ force: true }).then(function(obj) {
       fixtures.loadFile('fixtures/agents.json', models).then(function() {
-        browser.visit('/', done);
+        models.Agent.findOne().then(function(results) {
+          agent = results;
+          browser.visit('/', function() {
+            browser.assert.success();
+            done();
+          });
+        });
       });
     }).catch(function(error) {
       done.fail(error);
@@ -20,7 +27,6 @@ describe('login', function() {
   });
 
   it('shows the home page', function() {
-    browser.assert.success();
     browser.assert.text('h1', 'Accountant');
   });
 
@@ -28,7 +34,36 @@ describe('login', function() {
     browser.assert.attribute('form', 'action', '/login')
   });
 
-  it('does not display the login form if logged in', function() {
-    expect(browser.query("form[action='/login']")).not.toBeDefined();
+  it('does not display the logout button if not logged in', function() {
+    expect(browser.query("a[href='/logout']")).toBeNull();
+  });
+
+
+  describe('login process', function() {
+    beforeEach(function(done) {
+      browser
+        .fill('email', agent.email)
+        .fill('password', 'secret')
+        .pressButton('Login', function(err) {
+          browser.assert.success();
+          done();
+        });
+    });
+
+    it('does not display the login form', function() {
+      expect(browser.query("form[action='/login']")).toBeNull();
+    });
+
+    describe('logout', function() {
+      it('does not display the logout button if not logged in', function(done) {
+        browser
+          .clickLink('Logout', function(err) {
+            browser.assert.success();
+            expect(browser.query("a[href='/logout']")).toBeNull();
+            browser.assert.attribute('form', 'action', '/login')
+            done()
+          });
+      });
+    });
   });
 });

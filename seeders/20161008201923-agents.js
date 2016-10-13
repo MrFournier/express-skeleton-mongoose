8ca4a82216b392fd062ac362fbe878c314d78819
Daniel Bidulock <daniel@capitolhill.ca>
2016-10-13 16:18:13 -0400

    Correct table name on down function and add another seed

--- seeders/20161008201923-agents.js
+++ seeders/20161008201923-agents.js
@@ -4,13 +4,19 @@ var models = require('../models');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return models.Agent.bulkCreate([{
-      email: 'daniel@capitolhill.ca',
-      password: 'secret'
-    }], {individualHooks: true});
+    return models.Agent.bulkCreate([
+      {
+        email: 'daniel@example.com',
+        password: 'secret'
+      },
+      {
+        email: 'troy@example.com',
+        password: 'topsecret'
+      }
+    ], {individualHooks: true});
   },
 
   down: function (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Agent', null, {});
+    return queryInterface.bulkDelete('Agents', null, {});
   }
 };

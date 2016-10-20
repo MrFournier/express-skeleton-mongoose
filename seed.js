39b310555ebd95bfb9544f17972f6354f425943d
Daniel Bidulock <daniel@capitolhill.ca>
2016-10-20 01:44:24 -0400

    Exit process

--- seed.js
+++ seed.js
@@ -8,9 +8,11 @@ models.once('open', function() {
   seeder.seed(data, {dropCollections: true}).then(function(dbData) {
     // The database objects are stored in dbData
     console.log('DONE: ' + JSON.stringify(dbData));
+    process.exit(0);
   }).catch(function(err) {
     // handle error
     console.log('SEEDER ERROR: ' + err);
+    process.exit(1);
   });
 });
 
